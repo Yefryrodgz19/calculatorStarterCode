3238219157df2fb171ad0938feece414ad6ebabe
Yefry Rodriguez <yefry.r.student@lyonscommunityschool.org>
2017-04-26 13:49:57 +0000

    calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -2,36 +2,45 @@
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
-
+// names: Yefry and Stephanie 
 function divide(x,y) {
 	//Write your code below this line
-
+var divide = x/y;
+return divide;
 }
 
 function multiply(x,y) {
-
+var product= x*y;
+return product;
 }
 
 function subtract(x,y) {
-
+var subtract= x-y;
+return subtract;
 }
 
 function add(x,y) {
-
+var sum= x+y;
+return sum;
 }
 
 function power(x,y) {
-
+var power = x,y;
+return power;
 }
 
 function square_root(x) {
-
+var square_root= x,y;
+return square_root;
 }
 
 function logBase10(x) {
-
+var total = Math.log(x);
+return total;
 }
 
 function generateRandomNumber() {
+    var total =Math.floor(Math.random() *20);
+    return total;
 
 }
